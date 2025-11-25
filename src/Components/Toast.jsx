53b77f76b8add9e663e1eab62aa455e939f289be
Phoenix Paulina Schmid <paulina@gelbphoenix.de>
2025-11-25 19:54:27 +0100

    Changed Toast positioning to be bottom instead of top
    Signed-off-by: Phoenix Paulina Schmid <paulina@gelbphoenix.de>

--- src/Components/Toast.jsx
+++ src/Components/Toast.jsx
@@ -1,10 +1,10 @@
 import { Toast as ToastFB } from 'flowbite-react';
 
-const Toast = ({ content }) => {
+const Toast = ({ content, height }) => {
   if (content.title === null || content.title === '') return null;
 
   return (
-    <ToastFB className="fixed inset-x-6 top-[85dvh] z-10 outline outline-2 outline-stone-600 dark:outline-stone-300 text-[#001b5e] dark:text-stone-300">
+    <ToastFB className="fixed inset-x-6 bottom-[3%] z-10 outline outline-2 outline-stone-600 dark:outline-stone-300 text-[#001b5e] dark:text-stone-300">
       <div className="bg-[#001b5e] dark:bg-stone-300">{content.icon}</div>
       <div className="ml-3 flex flex-col">
         <span className="mb-1 text-lg font-bold">{content.title}</span>
