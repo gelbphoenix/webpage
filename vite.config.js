3abf1cbbd3376b936e6af669eee19f3d4de88f5b
Phoenix Paulina Schmid <paulina@gelbphoenix.de>
2025-12-12 19:44:52 +0100

    Added picture.webp to .assetsignore and added build config to vite config
    Signed-off-by: Phoenix Paulina Schmid <paulina@gelbphoenix.de>

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,23 @@ import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'esbuild',
+    copyPublicDir: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': '/src',
