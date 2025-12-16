5a3e9a6447193af6f02a27f2c4e79050dc1c0f63
Phoenix Paulina Schmid <paulina@gelbphoenix.de>
2025-12-16 16:48:29 +0100

    Added theme to EuropeanStars.jsx and changed colors for svgs
    Signed-off-by: Phoenix Paulina Schmid <paulina@gelbphoenix.de>

--- src/Components/Icons/EuropeanStars.jsx
+++ src/Components/Icons/EuropeanStars.jsx
@@ -1,12 +1,17 @@
 import EUStars from 'Assets/european-flag.svg?react';
+import { useContext } from 'react';
+import { ThemeContext } from 'Contexts/ThemeContext';
 
 export const EuropeanStars = ({ size }) => {
+  const { theme } = useContext(ThemeContext);
+  const color = theme ? '#D6D3D1' : '#001b5e';
+
   return (
     <EUStars
       width={size}
       height={size}
-      stroke="currentColor"
-      fill="currentColor"
+      stroke={color}
+      fill={color}
       role="img"
     />
   );
